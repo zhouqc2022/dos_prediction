60b5f5016f47d41c7e259832bb435710728e17e3
zhouqc <113653750+zhouqc2022@users.noreply.github.com>
2023-08-07 19:39:13 +0800

    Update nn.py

--- nn.py
+++ nn.py
@@ -1,5 +1,9 @@
 import os
+import glob
 import random
+from pymatgen.io.cif import CifParser
+import numpy as np
+from pymatgen.core.composition import Composition
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -7,20 +11,18 @@ from pymatgen.core.structure import Structure
 from pymatgen.io.vasp.inputs import Poscar
 from pymatgen.core import Element
 
-'''cif_folder是包含所有cif文件的文件夹'''
-def POSCAR_generator(cif_folder):
-    for filename in os.listdir(cif_folder):
-        if filename.endswith(".cif"):
-            cif_path = os.path.join(cif_folder, filename)
-            structure = Structure.from_file(cif_path)
-            name = os.path.splitext(filename)[0] + '.vasp'
-            poscar_path = os.path.join(cif_folder, name)
-            poscar = Poscar(structure)
-            poscar.write_file(poscar_path)
+# '''cif_folder is the file folder containing all cifs'''
+# def POSCAR_generator(cif_folder):
+#     cif_files_path = glob.glob(os.path.join(cif_folder, "*.cif"))
+#     for cif_file_path in cif_files_path:
+#         structure = Structure.from_file(cif_file_path)
+#         poscar_path = os.path.splitext(cif_file_path)[0] + '.vasp'
+#         Poscar(structure).write_file(poscar_path)
 
-'''folder是包含所有结构POSCAR文件的文件夹'''
-def data(folder):
-    file_names = os.listdir(folder)
+'''folder is the file folder containing all cifs'''
+def data(cif_folder, dos_file_path):
+    #输入文件
+    file_names = [file for file in os.listdir(cif_folder) if file.endswith('.cif')]
     random.shuffle(file_names)
     train_ratio = 0.7
     val_ratio = 0.2
@@ -33,68 +35,83 @@ def data(folder):
     train_file_names = file_names[:train_samples]
     val_file_names = file_names[train_samples:train_samples + val_samples]
     test_file_names = file_names[train_samples + val_samples:]
-    return train_file_names, val_file_names, test_file_names
 
-'''以Co_test为例generate input_list, [v_list]'''
-def feature_generator(file_names):
-    data_list = []
-    target_list =[]
-    for file_name in file_names:
-        file_path = os.path.join('Co_test\\vasp',file_name)
-        with open(file_path, 'r') as file:
-            lines = file.readlines()
-        name = file_path.split('\\')[-1].split('.')[0]
-        input = []  #一个 样本的输入
-        for i in lines[8:]:  # lines[7]为direct
-            u = i.strip().split()  # k = ['0.0000000000000000', '0.0000000000000000', '0.0000000000000000', 'Si']
-            element = Element(u[3])
-            z = element.Z  #原子序数
-            try:
-                valence = element.valence
-                valence_num = sum(valence)
-            except:
-                valence_num = 0
-            u.append(z)
-            u.append(valence_num)   #价电子数
-            del u[3]
-            u_list = [float(x) for x in u]
-            input.append(u_list)
-        while len(input) < 100:
-            input.append([0.0, 0.0, 0.0, 0.0, 0.0])  # 填充全为0的5维向量
-        input = [item for sublist in input for item in sublist]
-        # 获得输出向量v
-        with open('Co_test\\Co_dos_100.csv', 'r') as file:
-            vectors = file.readlines()
-        for i in vectors:
-            if i.startswith(name):
-                a = i.split('csv,')[1]
-                b = a[2:]
-                c = b[:-3]
-                dos = [float(x) for x in c.split(',')]
-        data_list.append(input)
-        target_list.append([dos])
-    return data_list,target_list
+    train_data = []
+    train_targets = []
+    for i in train_file_names:
+        path = os.path.join(cif_folder, i)
+        input, dos = feature_generator(path, dos_file_path)
+        train_data.append(input)
+        train_targets.append(dos)
 
-class nnetwork(nn.Module):
-    def __init__(self):
-        super(nnetwork, self).__init__()
-        self.fc1 = nn.Linear(500, 200)
-        self.fc2 = nn.Linear(200, 200)
-        self.fc3 = nn.Linear(200, 200)
+    val_data = []
+    val_targets = []
+    for i in val_file_names:
+        path = os.path.join(cif_folder, i)
+        input, dos = feature_generator(path, dos_file_path)
+        val_data.append(input)
+        val_targets.append(dos)
 
-    def forward(self, x):
-        x = torch.relu(self.fc1(x))
-        x = torch.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+    test_data = []
+    test_targets = []
+    for i in test_file_names:
+        path = os.path.join(cif_folder, i)
+        input, dos = feature_generator(path, dos_file_path)
+        test_data.append(input)
+        test_targets.append(dos)
 
-train_file_names, val_file_names, test_file_names = data()
+    return train_data, train_targets, val_data,\
+           val_targets, test_data, test_targets
+'''从poscar文件里得到列表input, 从dos.csv文件里得到dos,返回两个list: 输入input_data和输出dos'''
+def feature_generator(cif_file_path, dos_file_path):
+    name = cif_file_path.split('\\')[-1].split('.')[0]
+    input = []
+    dos = []
+    cif_parser = CifParser(cif_file_path)
+    structure = cif_parser.get_structures(primitive=False)[0]
+    # 获取sites
+    sites = structure.sites
+    # 获取坐标
+    for site in sites:
+        coordination = site.coords
+        element = site.specie
+        z = element.Z
+        try:
+            valence = element.valence
+            valence_num = sum(valence)
+        except:
+            valence_num = z
 
-train_data, train_targets = feature_generator(train_file_names)
-val_data, val_targets = feature_generator(val_file_names)
-test_data, test_targets = feature_generator(test_file_names)
+        try:
+            x = element.X
+        except:
+            x = 2.2
 
 
+        a = np.append(coordination, float(z))
+        b = np.append(a, float(valence_num))
+        c = np.append(b, float(x))
+
+
+        input.append(c)
+    while len(input) < 100:
+        input.append(np.zeros(6,dtype=float))   # 填充全为0的6维向量
+    input = [item for sublist in input for item in sublist]
+
+    with open(dos_file_path, 'r') as file:
+        vectors = file.readlines()
+    for i in vectors:
+        if i.startswith(name):
+            a = i.split('csv,')[1]
+            b = a[2:]
+            c = b[:-3]
+            d = [float(x) for x in c.split(',')]
+            dos.append(d)
+    return input, dos
+'''input is list of array, dos is a list'''
+
+
+train_data, train_targets, val_data, val_targets, test_data, test_targets = data('Co_test', 'Co_test\\Co_dos_100.csv')
 train_data = torch.tensor(train_data)
 train_targets = torch.tensor(train_targets)
 val_data = torch.tensor(val_data)
@@ -102,23 +119,41 @@ val_targets = torch.tensor(val_targets)
 test_data = torch.tensor(test_data)
 test_targets = torch.tensor(test_targets)
 
-model = nnetwork()
-criterion = nn.MSELoss()
-optimizer = optim.SGD(model.parameters(), lr=0.01)
+class CustomLoss(nn.Module):
+    def __init__(self):
+        super(CustomLoss, self).__init__()
 
+    def forward(self, output, target):
+        diff = output - target
+        loss = torch.mean(torch.norm(diff) / torch.norm(target))
+        return loss
+
+class nnetwork(nn.Module):
+    def __init__(self):
+        super(nnetwork, self).__init__()
+        self.fc1 = nn.Linear(600, 600, dtype=torch.float64)
+        self.fc2 = nn.Linear(600, 200, dtype=torch.float64)
+        self.fc3 = nn.Linear(200, 200, dtype=torch.float64)
+
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+
+model = nnetwork()
+criterion =  CustomLoss()
+# criterion = nn.MSELoss()
+optimizer = optim.SGD(model.parameters(), lr=0.01)
 epochs = 100
 for epoch in range(epochs):
-
     optimizer.zero_grad()
-
     output = model(train_data)
-
     loss = criterion(output, train_targets)
-
     loss.backward()
-
     optimizer.step()
-
     if (epoch + 1) % 1 == 0:
         print(f"Epoch {epoch + 1}/{epochs}, Loss: {loss.item()}")
 #在验证集上评估模型
@@ -131,7 +166,6 @@ with torch.no_grad():
     test_output = model(test_data)
     test_loss = criterion(test_output, test_targets)
     print(f"Test Loss: {test_loss.item()}")
-
 
 save_path = 'model.pth'
 torch.save(model.state_dict(), save_path)
