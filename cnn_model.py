cd4755494cb3d73b8cd61b21f7ea5f346a2955fe
zhouqc <113653750+zhouqc2022@users.noreply.github.com>
2024-05-24 22:00:03 +0800

    Update cnn_model.py

--- cnn_model.py
+++ cnn_model.py
@@ -7,15 +7,8 @@ from torch.nn import Sequential, Linear, BatchNorm1d, PReLU
 import torch_geometric
 from torch_geometric.typing import PairTensor, Adj, OptTensor, Size
 from torch_geometric.nn.conv import MessagePassing
-# from torch_scatter import scatter_mean, scatter_add, scatter_max, scatter
+import torch.nn as nn
 
-from torch_geometric.nn import (
-    Set2Set,
-    global_mean_pool,
-    global_add_pool,
-    global_max_pool,
-    CGConv,
-)
 
 
 # GNN model
@@ -23,10 +16,10 @@ class DOSpredict(torch.nn.Module):
     def __init__(
             self,
             data,
-            dim1=128,
-            dim2=128,
-            pre_fc_count=1,
-            gc_count=3,
+            dim1=64,
+            dim2=64,
+            pre_fc_count=0,
+            gc_count=1,
             batch_norm="True",
             batch_track_stats="True",
             dropout_rate=0.0,
@@ -43,28 +36,34 @@ class DOSpredict(torch.nn.Module):
 
         ##Determine gc dimension dimension
         assert gc_count > 0, "Need at least 1 GC layer"
-        '''如果没有预处理，则卷积层的输入维度为data.num_features'''
+
+        #determine  gc dimension
         if pre_fc_count == 0:
-            print('预处理层不能为0')
+            self.gc_dim = data.num_features
         else:
             self.gc_dim = dim1
+
         ##Determine post_fc dimension
         if pre_fc_count == 0:
-            print('预处理层不能为0')
+            post_fc_dim = data.num_features   #data.num_features一般为112
         else:
             post_fc_dim = dim1
 
 
 
-
         ##Determine output dimension length
-        output_dim = len(data[0].y[0])
+        if data[0].y.ndim == 0 :  #此时目标值为标量
+            output_dim = 1 #len(data[0].y[0])
+        else:
+            output_dim = len(data[0].y[0])
 
-        '''data.num_features = 112'''
-        ##Set up pre-GNN dense layers
+        print('网络的输出维度为{}'.format(output_dim))
+
+
+        ##pre_lin_list
         if pre_fc_count > 0:
             self.pre_lin_list = torch.nn.ModuleList()
-            for i in range(pre_fc_count):  #pre_fc_count=1时只会执行一次
+            for i in range(pre_fc_count):
                 if i == 0:
                     lin = Sequential(torch.nn.Linear(data.num_features, dim1), torch.nn.PReLU())  #创建一个线性层（torch.nn.Linear）和激活函数（torch.nn.PReLU）的序列（Sequential）
                     self.pre_lin_list.append(lin)  #添加到前馈神经网络的 self.pre_lin_list 中
@@ -85,19 +84,20 @@ class DOSpredict(torch.nn.Module):
                 bn = BatchNorm1d(self.gc_dim, track_running_stats=self.batch_track_stats, affine=True)  #self.batch_track_stats=’True'时会在训练过程中持续追踪整个数据集的均值和方差，常用于训练时
                 self.bn_list.append(bn)
 
-
+        #感知层dos_mlp
         self.dos_mlp = Sequential(Linear(post_fc_dim, dim2),
                                   torch.nn.PReLU(),
                                   Linear(dim2, output_dim),
-                                  torch.nn.PReLU(),)
+                                  torch.nn.PReLU(),
+                                  BinaryOutputNet(200, 200))
     def forward(self, data):
-        ##Pre-GNN dense layers
+        #Pre-GNN dense layers
         for i in range(0, len(self.pre_lin_list)):
             if i == 0:
                 out = self.pre_lin_list[i](data.x)
             else:
                 out = self.pre_lin_list[i](out)
-        ##GNN layers，默认batch_norm = ’True'
+        #GNN layers，默认batch_norm = ’True'
         for i in range(0, len(self.conv_list)):
             if len(self.pre_lin_list) == 0 and i == 0:
                 if self.batch_norm == "True":
@@ -111,9 +111,11 @@ class DOSpredict(torch.nn.Module):
                     out = self.bn_list[i](out)
                 else:
                     out = self.conv_list[i](out, data.edge_index, data.edge_attr)
-        out = F.dropout(out, p=self.dropout_rate, training=self.training)
+        out = F.dropout(out, p=self.dropout_rate, training=self.training)  #正则化技术，用于减少过拟合。它在训练过程中随机地将输入张量的部分元素设为零，以一定的概率 self.dropout_rate，而在测试时则保持所有元素不变
         ##Post-GNN dense layers
         dos_out = self.dos_mlp(out)  #one last linaer layer + output layer
+
+
         return dos_out
 
 '''one graph convolution layer'''
@@ -149,7 +151,16 @@ class GC_block(MessagePassing):
         return z
 
 
-# if __name__ == '__main__':
-#     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-#     model = DOSpredict().to(device)
-#     print(summary(model, (72, 112)))
+class BinaryOutputNet(nn.Module):
+    def __init__(self, input_size, output_size):
+        super(BinaryOutputNet, self).__init__()
+        self.fc1 = nn.Linear(input_size, 128)
+        self.fc2 = nn.Linear(128, output_size)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = self.fc2(x)
+        x = self.sigmoid(x)
+        x = torch.round(x)
+        return x
